d91593f4705d2b5197abef149ef6ef5c87833211
Sergey Volodin <etoestja@yandex.ru>
2016-12-16 21:04:24 +0300

    Update rank example

---
+++ code/draw_cbad_R4/example01_Rank.m
@@ -0,0 +1,35 @@
+clear all;
+
+load('example03.mat');
+
+% basis: c_+A=I, c_+b=0
+[A_, b_] = change_basis(A, b, c_plus);
+
+R = complete_basis(c_plus);
+
+Phi = [];
+Theta = [];
+M = [];
+
+Sx = 500;
+Sy = 500;
+
+theta_r = [0:Sx-1] * pi / Sx;
+phi_r = [0:Sy-1] / Sy * 2 * pi;
+
+for theta = theta_r
+    for phi = phi_r
+        c0 = [sin(theta) * cos(phi); sin(theta) * sin(phi); cos(theta)];
+        c = R * c0;
+        Ac = get_Ac(A_, c);
+        r = eig(Ac);
+        a = repmat(r, 1, 4);
+        m = min(min(abs(a - a') + diag(repmat([nan], 1, 4))));
+        M(end + 1) = m;
+       % fprintf('m = %f\n', m);
+    end
+end
+
+Z = reshape(M, Sy, Sx);
+
+surf(theta_r, phi_r, Z);
