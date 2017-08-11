8b4d2fff97aa9bdf96d54d6a55df953ac987cbfb
Sergei Volodin <etoestja@yandex.ru>
2017-08-11 23:53:17 +0300

    Add z_max_guess parameter instead of y

--- get_z_max.m
+++ get_z_max.m
@@ -1,5 +1,5 @@
-function z_max = get_z_max(A, b, y, c_plus, k, DEBUG)
-%% z_max = get_z_max(A, b, y, c_plus, k)
+function z_max = get_z_max(A, b, c_plus, z_max_guess, k, DEBUG)
+%% z_max = get_z_max(A, b, c_plus, z_max_guess, k)
 % obtain z_max = inf z(c) over C_- using at most
 % k iterations of get_c_minus
 %
@@ -7,23 +7,27 @@ function z_max = get_z_max(A, b, y, c_plus, k, DEBUG)
 % matrices (A_1, ..., A_m) -> tensor A(i, j, k) -- i'th row, j'th column of matrix A_k
 % vectors  (b_1, ..., b_m) -> tensor b(i, j)    -- i'th element          of vector b_j
 %
+% y = y_0 - z_max_guess * c_plus
+% where y_0 is the boundary point which is touched by hyperplane c_plus
+%
 %% example
 % 1) loading map from file
 % 2) obtaining c_plus for map
-% 3) generating point y inside image F
-% 4) minimizing z(c) over c_- using k = 1 starting point
+% 3) minimizing z(c) over c_- using k = 1 starting point
 %
 % clear all;
 % load('maps/real_R4_R4.mat');
 % c_plus = get_c_plus(A, 10, 1);
-% x = [1 1 0 0]';
-% y = quadratic_map(A, b, x);
-% get_z_max(A, b, y, c_plus, 10)
+% get_z_max(A, b, c_plus, 0.01, 10)
 
 %%
     if nargin == 5
         DEBUG = 0;
     end
+
+%%
+    x = -get_Ac(A, c_plus) * get_Ac(b, c);
+    y = quadratic_map(A, b, x) - z_max_guess * c_plus;
 
 %% basis: c_+A=I, c_+b=0
     [A_, b_, ~, y0] = change_basis(A, b, c_plus);
