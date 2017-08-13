80588b7d2ce3cbcbd11495da0eff0a86d5c40580
Sergei Volodin <etoestja@yandex.ru>
2017-08-14 00:18:10 +0300

    Add figure #25

--- examples/figures/draw_cbad_R4/rg_n_2.m
+++ examples/figures/draw_cbad_R4/rg_n_2.m
@@ -29,15 +29,22 @@ for i=1:N
         norm(Q_inv), rank(Q, 1e-5), z, c(1), c(2), c(3), lambda, cminus_distance_c);
 end
 
-start_id = N;
-step_size = 0.02;
-delta_c = c_dot_array(:, start_id) * step_size;
+start_id = N - 3;
+dist_to_end = norm(c_item_array(:, N)-c_item_array(:, start_id));
 c = c_item_array(:, start_id);
+max_step = dist_to_end / 3;
 
-[c_new, lambda] = project_descent(A_, b_, c + delta_c, normal, normal, 1);
+K = 20;
+for i=0:K
+    step_size = i * max_step / K;
+    delta_c = c_dot_array(:, start_id) * step_size;
+    [c_new, lambda] = project_descent(A_, b_, c + delta_c, normal, normal, 1);
+    c_item_array(:, end + 1) = c_new;
+    c_new_dist_to_end = norm(c_new-c_item_array(:,N));
+    fprintf('iteration %d step_size %f dist %f\n', i, step_size, c_new_dist_to_end);
+end
 
-c_item_array(:, end + 1) = c_new;
-c_dot_array(:, end + 1) = zeros(m, 1);
+N_new = size(c_item_array, 2);
 
 c_item_array_p = R * c_item_array;
 c_dot_array_p = (R * c_dot_array);
@@ -51,8 +58,12 @@ axis equal;
 %set(s, 'FaceColor', [0 0 0], 'FaceAlpha', 0.1);
 %set(s, 'EdgeColor', [0 0 0], 'EdgeAlpha', 0.3)
 
-plot3(c_item_array_p(1, :), c_item_array_p(2, :), c_item_array_p(3, :));
+plot_c = plot3(c_item_array_p(1, :), c_item_array_p(2, :), c_item_array_p(3, :));
 scatter3(c_item_array_p(1, :), c_item_array_p(2, :), c_item_array_p(3, :));
-quiver3(c_item_array_p(1, :), c_item_array_p(2, :), c_item_array_p(3, :), c_dot_array_p(1, :), c_dot_array_p(2, :), c_dot_array_p(3, :), 0.3);
-scatter3(c_item_array_p(1, N + 1), c_item_array_p(2, N + 1), c_item_array_p(3, N + 1), 100, [1 0 0], 'Marker', 'hexagram');
-scatter3(c_item_array_p(1, start_id), c_item_array_p(2, start_id), c_item_array_p(3, start_id), 100, [1 0 0], 'Marker', 'diamond');
+plot_end = scatter3(c_item_array_p(1, N + 1 : N_new), c_item_array_p(2, N + 1 : N_new), c_item_array_p(3, N + 1 : N_new), 100, [1 0 0], 'Marker', 'hexagram');
+plot_c_dot = quiver3(c_item_array_p(1, 1:N), c_item_array_p(2, 1:N), c_item_array_p(3, 1:N), c_dot_array_p(1, 1:N), c_dot_array_p(2, 1:N), c_dot_array_p(3, 1:N), 1);
+%plot_end = scatter3(c_item_array_p(1, N + 1), c_item_array_p(2, N), c_item_array_p(3, N + 1), 100, [1 0 0], 'Marker', 'hexagram');
+plot_start = scatter3(c_item_array_p(1, start_id), c_item_array_p(2, start_id), c_item_array_p(3, start_id), 100, [1 0 0], 'Marker', 'diamond');
+
+leg = legend([plot_c, plot_c_dot, plot_start, plot_end], {'$c$', '$\dot{c}$', '$c_k$', '$\hat{c}_{k+1}$'});
+set(leg,'Interpreter','latex')
