cc349af1acf136205d3c59d2575fc30b676da506
Sergei Volodin <etoestja@yandex.ru>
2017-08-15 01:44:03 +0300

    Add video to README

--- examples/figures/draw_cbad_R4/max_c_plus.m
+++ examples/figures/draw_cbad_R4/max_c_plus.m
@@ -1,6 +1,6 @@
 clear all;
 load('example02.mat');
-k = 150;
+k = 200;
 z_max_guess = 0.5;
 n = size(A, 1);
 m = size(A, 3);
@@ -11,7 +11,7 @@ c_plus_max = get_max_c_plus(A);
 z_max_max = get_z(c_plus_max);
 
 %%
-[SX, SY, SZ] = sphere(2);
+[SX, SY, SZ] = sphere(15);
 SX = SX(:);
 SY = SY(:);
 SZ = SZ(:);
@@ -39,3 +39,4 @@ for i = 1:N
     z_max_array(i) = get_z(c_plus_array(:, i));
     waitbar(1. * i / N, h, sprintf('z_{max} %d/%d', i, N));
 end
+close(h);
