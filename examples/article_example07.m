9db2db8e8cbe903aabe404f1e7fe78dad5bb8d5c
Sergei Volodin <etoestja@yandex.ru>
2017-09-01 00:40:43 +0300

    Add example 7 code #31

---
+++ examples/article_example07.m
@@ -0,0 +1,32 @@
+clear all;
+
+% changing cwd to directory of .m file
+cd(fileparts(which(mfilename)));
+
+% loading map
+load('./maps/article_example07_R4_R4.mat');
+
+disp('1. The map R4->R4');
+for i = 1:n
+    fprintf('A_%d = %s\n', i, mat2str(A(:, :, i)));
+end
+
+for i = 1:n
+    fprintf('b_%d = %s\n', i, mat2str(b(:, i)));
+end
+
+%%
+rand_seed = 60;
+rng(rand_seed);
+fprintf('3. Random seed set to %d\n', rand_seed);
+
+%%
+c_plus = get_c_plus(A, 10, 1);
+
+fprintf('2. c_+ = %s\n', mat2str(c_plus));
+
+%%
+fprintf('4. Finding convex cut\n');
+z_max = get_z_max(A, b, c_plus, 10, 100, 1);
+
+fprintf('Result: convex cut of size %f\n', z_max);
