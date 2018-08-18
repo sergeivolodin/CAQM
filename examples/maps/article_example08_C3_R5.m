0f885dc84a916641a873dc502690c2db6e4e3289
Sergei Volodin <sergei.volodin@epfl.ch>
2018-08-18 16:43:50 +0200

    Add example08 map m/mat

---
+++ examples/maps/article_example08_C3_R5.m
@@ -0,0 +1,34 @@
+%%
+clear all;
+
+% changing cwd to directory of .m file
+cd(fileparts(which(mfilename)));
+
+%% defining map
+% dimensions
+n = 3;
+m = 5;
+is_complex = 1;
+
+rng(47, 'twister');
+
+a_max = 1;
+A = zeros(n, n, m);
+
+for j = 1 : m
+    A(:, :, j) = randi([-a_max a_max], n, n) + randi([-a_max a_max], n, n) * i;
+    A(:, :, j) = (A(:, :, j) + A(:, :, j)');
+end
+
+% adding a bit of E to ensure existence of c_plus
+A(:, :, m) = A(:, :, m) + 5 * a_max * eye(n);
+
+b = randi([-a_max, a_max], n, m);
+
+%% calculating c_plus
+c_plus = get_c_plus(A);
+disp('=== c_plus:');
+disp(c_plus');
+
+%% saving results
+save('article_example08_C3_R5.mat', 'A', 'b', 'n', 'm', 'is_complex', 'c_plus');
