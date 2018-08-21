d5b8d9c13de56b52f6d7d0e2abd5fed655bb9c88
Sergei Volodin <sergei.volodin@epfl.ch>
2018-08-22 00:06:25 +0200

    Fix comment

--- library/point_inside.m
+++ library/point_inside.m
@@ -1,7 +1,7 @@
 function y = point_inside(A, b, c_plus, z)
 %% y = point_inside(A, b, c_plus, z)
 % calculate the point y inside the image F
-% as y = y0 - z * c_plus
+% as y = y0 + z * c_plus
 % where y0 is a touching point for c_plus
 
 %% initialization
