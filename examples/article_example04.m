fa419c17aead028c13854e7a9732a3754e8516c4
Sergei Volodin <sergei.volodin@epfl.ch>
2018-08-22 00:30:08 +0200

    Fix ex04

--- examples/article_example04.m
+++ examples/article_example04.m
@@ -16,17 +16,21 @@ for i = 1:m
 end
 
 %%
-rand_seed = 60;
+rand_seed = 2;
 rng(rand_seed);
 fprintf('3. Random seed set to %d\n', rand_seed);
 
-%%
-c_plus = get_c_plus(A, 10, 1);
+%% obtained s.t. point y0+c_plus*z_guess is close to the nonconvexity at (0, 0)
+c_plus = [0.7991 -0.3533 0.3924 0.2876]';
 
 fprintf('2. c_+ = %s\n', mat2str(c_plus));
 
 %%
 fprintf('4. Finding convex cut\n');
-z_max = get_z_max(A, b, c_plus, 20, 500, 1);
+z_max = get_z_max(A, b, c_plus, 1.7901, 500, 1);
 
+% Note that there are two vectors c_-: (0 1 0 0) -- analytically correct
+% And another one, which is not really a c_- (numerical error for checking
+% rank -- matrix is near degenerate and is considered degenerate, which is
+% wrong.
 fprintf('Result: convex cut of size %f\n', z_max);
