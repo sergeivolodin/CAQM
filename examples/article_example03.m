8ea8030242d850eed5f798670b88cc976260e71a
Sergei Volodin <sergei.volodin@epfl.ch>
2018-08-23 21:56:49 +0200

    Add ex2 nb

--- examples/article_example03.m
+++ examples/article_example03.m
@@ -15,6 +15,6 @@ display(c_plus');
 rng(41);
 
 %% cutting the convex part
-z_max = get_z_max(A, b, c_plus, 0.1 , 200, 1);
+z_max = get_z_max(A, b, c_plus, 0.1, 200, 1);
     
 fprintf('Result: convex subpart, z_max = %f\n', z_max);
