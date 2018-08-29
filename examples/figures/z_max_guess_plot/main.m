d49a0f7795eeb16918c268f0e8aa4d577053b0c1
Sergei Volodin <sergei.volodin@epfl.ch>
2018-08-29 23:29:17 +0200

    Add c frequency over guess and z_min plot

--- examples/figures/z_max_guess_plot/main.m
+++ examples/figures/z_max_guess_plot/main.m
@@ -1,22 +1,23 @@
+%%
 clear all;
 
 % changing cwd to directory of .m file
 cd(fileparts(which(mfilename)));
 
 % loading map
-load('../../maps/article_example06_R4_R4.mat');
+load('../../maps/article_example05_R4_R4.mat');
 
 % for minimizing z
 [A_, b_, ~, y0] = change_basis(A, b, c_plus);
 
 % name for the output file
-name = 'ex06_zc_big';
+name = 'ex05_zc_big';
 
 % number of trials per one z
 trials = 1500;
 
 % array of zs (change with name)
-z_array = linspace(0, 0.5, 20);
+z_array = linspace(0, 500000, 20);
 
 %% fixing the random seed
 rng(10);
@@ -84,4 +85,54 @@ end
 save(sprintf('results_%s.mat', name), 'ncvx', 'z_array', 'z_min_array', 'ncvx_c', 'trials');
 
 %% plotting the array
-scatter(z_array, ncvx / trials);
+%clear all;
+%name = 'ex06_zc_big';
+%load(sprintf('results_%s.mat', name));
+
+%%
+z_nonzero = [];
+for z_min = reshape(z_min_array, 1, [])
+    if z_min > 0
+        z_nonzero(end + 1) = z_min;
+    end
+end
+
+%%
+precision = 0.02;
+unique_z = uniquetol(z_nonzero, precision);
+
+figure;
+hold on;
+
+labels = {};
+plots = [];
+
+plots(end + 1) = plot(z_array, ncvx / trials);
+labels{end + 1} = 'Total';
+
+s = 1;
+for z_min = unique_z
+    k = 1;
+    ncvx_list = [];
+    for z = z_array
+        sz = ncvx(k);
+        current_z = z_min_array(k, 1 : sz);
+        current_this_z_min = 0;
+        for cz = current_z
+            [~, v] = min(abs(cz - unique_z));
+            if v == s
+                current_this_z_min = current_this_z_min + 1;
+            end
+        end
+        ncvx_list(end + 1) = current_this_z_min / trials;
+        k = k + 1;
+    end
+    s = s + 1;
+    plots(end + 1) = plot(z_array, ncvx_list);
+    labels{end + 1} = sprintf('zmin=%.4f', z_min);
+end
+
+title('Frequency of c over guesses');
+xlabel('z_{\rm max}^{\rm guess}');
+ylabel('Frequency of c');
+legend(plots, labels);
