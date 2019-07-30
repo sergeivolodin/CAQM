73f66bdda94c2ea517c182f2910844d41a4fe084
Sergei Volodin <sergei.volodin@epfl.ch>
2019-07-30 18:03:58 +0200

    Add homogeneous real cert. idea

---
+++ library/homogeneous_real_nonconvexity_certificate.m
@@ -0,0 +1,42 @@
+function c = homogeneous_real_nonconvexity_certificate(A)
+%% c = homogeneous_real_nonconvexity_certificate(A)
+% obtain vector c s.t. lambda_min(c*A) -> max
+% and Tr(c*A)=0
+% and c ' * c == 1
+
+%% initializing
+    c = [];
+     
+    % dimensions
+    m = size(A, 3);
+    n = size(A, 1);
+    
+    % only works for real maps
+    assert(isreal(A));
+
+    cvx_clear;
+    cvx_quiet true;
+    
+%% looking for c_plus
+    
+    cvx_clear;
+    cvx_begin
+        variable gam;
+        variable c(m);
+        maximize(gam);
+        Ac = get_Ac(A, c);
+        sum(c) == 1;
+        Ac - gam * eye(n) == hermitian_semidefinite(n);
+        trace(Ac) == 0;
+    cvx_end
+     
+    % checking feasibility
+    if cvx_optval >= Inf
+        error('Not found');
+    end
+
+    disp(gam)
+    disp(cvx_status)
+     
+    c = c;
+end
