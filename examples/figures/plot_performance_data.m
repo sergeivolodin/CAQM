76306355dc7cabf39b9878366a12ce1e15d5f605
Sergei Volodin <sergei.volodin@epfl.ch>
2020-07-21 13:31:35 +0200

    Add data for higher dims

--- examples/figures/plot_performance_data.m
+++ examples/figures/plot_performance_data.m
@@ -3,29 +3,32 @@ nmax = 20;
 repetitions = 10;
 k = 100;
 L = length(nmin:nmax);
-results = zeros(L, repetitions, 2);
-
-
+%results = zeros(L, repetitions, 3);
 
 i = 1;
 for n = nmin:nmax
     for j = 1:repetitions
-        [time, found_c] = measure_performance(n, n, k);
         fprintf("n=m=%d rep=%d time=%.2f found_c=%d\n\n", n, j, time, found_c);
-        results(i, j, :) = [time, found_c];
+
+        if results(i, j, 1) ~= 0
+            fprintf("Skipping");
+            continue
+        end
+        [time, found_c, z_max] = measure_performance(n, n, k);
+        results(i, j, :) = [time, found_c, z_max];
     end
     i = i + 1;
 end
 
 save('plot_performance', 'nmin', 'nmax', 'repetitions', 'k', 'L', 'results');
 
-function [time, found_c] = measure_performance(n, m, k)
+function [time, found_c, z_max] = measure_performance(n, m, k)
     global c_array_export;
     [A, b] = get_random_f(n, m);
     tic
         c_plus = get_max_c_plus(A);
         z_max_guess = 10 * trace(get_Ac(A, c_plus));
-        get_z_max(A, b, c_plus, z_max_guess, k, k);
+        z_max = get_z_max(A, b, c_plus, z_max_guess, k, 1);
     time = toc;
 
     found_c = 0;
